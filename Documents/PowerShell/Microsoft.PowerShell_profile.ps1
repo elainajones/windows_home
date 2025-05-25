57521291a42513b8d652f090eb881dd3d5617039
Elaina Jones <133198355+elainajones@users.noreply.github.com>
2025-05-25 06:30:28 -0700

    feat: Update PowerShell profile

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,21 @@
-# Track in git
+$env:PATH="$env:PATH;C:\opt\vim-9.1.0\vim91\"
+$env:PATH="$env:PATH;C:\opt\MinGit-2.49.0\cmd\"
+Remove-Item Alias:wget -Force
+
 function explorer {
     explorer.exe .
 }
-Set-Alias -Name e -Value explorer
+function wget {
+    param (
+        $url
+    )
+    $ProgressPreference = 'SilentlyContinue'
+    Invoke-WebRequest -Uri "$url" -OutFile "$(split-path \"$url\" -leaf)"
+    Write-Host "Saved to $(split-path \"$url\" -leaf)"
+}
+
 Set-Alias -Name c -Value clear
+Set-Alias -Name e -Value explorer
 Set-Alias -Name npp -Value "C:/Program Files/Notepad++/notepad++.exe"
 
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -ScriptBlock {
