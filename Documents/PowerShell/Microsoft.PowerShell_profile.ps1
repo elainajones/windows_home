a7290d40dcc020565cb92b7d0846ed36d968c08b
Elaina Jones <133198355+elainajones@users.noreply.github.com>
2025-05-25 09:07:27 -0700

    refactor: wget alias for PowerShell

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,23 +1,41 @@
-$env:PATH="$env:PATH;C:\opt\vim-9.1.0\vim91\"
-$env:PATH="$env:PATH;C:\opt\MinGit-2.49.0\cmd\"
-Remove-Item Alias:wget -Force
+# For reference
+# $env:PATH="$env:PATH;C:\opt\vim-9.1.0\vim91\"
+# $env:PATH="$env:PATH;C:\opt\MinGit-2.49.0\cmd\"
+#
+# Remove any pre-existing system alias so we can override it
+# Remove-Item Alias:wget -Force
 
 function explorer {
-    explorer.exe .
+    param (
+        $Path = $PWD
+    )
+    explorer.exe $Path
 }
 function wget {
     param (
-        $url
+        [string]$Url,
+        [string]$OutputPath = $(Split-Path -Leaf $Url)
     )
+    if (Test-Path -PathType Container $OutputPath) {
+        $Path = "${Path}\$(Split-Path -Leaf $Url)"
+    } else {
+        # Make sure parent path exists.
+        New-Item -ItemType Directory -Path $(Split-Path -Parent $OutputPath) -Force > $null
+    }
+
+    Write-Host "Downloading $(Split-Path -Leaf $Url)"
+
     $ProgressPreference = 'SilentlyContinue'
-    Invoke-WebRequest -Uri "$url" -OutFile "$(split-path \"$url\" -leaf)"
-    Write-Host "Saved to $(split-path \"$url\" -leaf)"
+    Invoke-WebRequest -Uri $Url -OutFile $OutputPath
+
+    Write-Host "Saved to $OutputPath"
 }
 
 Set-Alias -Name c -Value clear
 Set-Alias -Name e -Value explorer
 Set-Alias -Name npp -Value "C:/Program Files/Notepad++/notepad++.exe"
 
+# Set ctrl-d to behave as 'exit' just like in Bash
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -ScriptBlock {
     [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
     [Microsoft.PowerShell.PSConsoleReadLine]::Insert('exit')
