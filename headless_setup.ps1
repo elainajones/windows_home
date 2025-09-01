9053d8c9c39328a1dd11ed606c814a5cc9b0770c
Elaina Jones <133198355+elainajones@users.noreply.github.com>
2025-08-31 17:35:53 -0700

    feat: Add install steps for OpenSSH server

--- headless_setup.ps1
+++ headless_setup.ps1
@@ -39,8 +39,35 @@ function New-TemporaryDirectory {
     New-Item -ItemType Directory -Path (Join-Path $tmp $name)
 }
 
+function Add-OpenSSH {
+    foreach ($i in Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*') {
+        # Install the OpenSSH Server
+        Add-WindowsCapability -Online -Name $i.name
+    }
+
+    # Start the sshd service
+    Start-Service sshd
+
+    # OPTIONAL but recommended:
+    Set-Service -Name sshd -StartupType 'Automatic'
+
+    # Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
+    if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+        Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
+        New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+    } else {
+        Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+    }
+}
+
 function main {
     $savePath = "C:\opt"
+
+    # Allow running scripts (this might need to be run before running this
+    Set-ExecutionPolicy -ExecutionPolicy ByPass
+
+    # Install and run SSH.
+    Add-OpenSSH
 
     # Make sure profile exists.
     New-Item -ItemType Directory -Path $savePath -Force > $null
@@ -135,6 +162,5 @@ function main {
     Remove-Item -Recurse -Force -Path $tempDir > $null
 
     Write-Host "Finished copying configuration files"
-
 }
 main
